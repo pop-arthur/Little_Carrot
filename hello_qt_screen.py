29579b8b9964c98dc145924b696a6a785f117d16
st10-01-16 <artu.200505@gmail.com>
2022-01-01 17:30:20 +0500

    create hello screen with Qt without database

---
+++ hello_qt_screen.py
@@ -0,0 +1,148 @@
+from PyQt5 import QtGui, uic, QtMultimedia, QtCore
+from PyQt5.QtCore import *
+from PyQt5.QtMultimedia import *
+from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QApplication, QWidget, QDialog, QTableWidgetItem
+import sys
+
+
+# функция проверяет есть ли данное имя в бд и возвращает True или False
+def player_name_exists(player_name):
+    pass
+
+
+# добавляет имя игрока в бд (регистрация)
+def add_player_name(player_name):
+    pass
+
+
+class HelloScreen(QWidget):
+    def __init__(self):
+        super(HelloScreen, self).__init__()
+        uic.loadUi('data/qt/hello_widget.ui', self)
+        self.init_ui()
+        self.show()
+
+    def init_ui(self):
+        self.setFixedSize(1000, 800)
+        self.setWindowTitle("Little Carrot")
+
+        self.account_settings_widget = None
+
+        self.continue_game_button.clicked.connect(self.continue_game)
+        self.new_game_button.clicked.connect(self.new_game)
+        self.account_button.clicked.connect(self.account_settings)
+        self.exit_button.clicked.connect(self.exit_clicked)
+        self.update_current_player()
+
+    def continue_game(self):
+        # запустить игру, подгрузив текущий прогресс игрока
+        pass
+
+    def new_game(self):
+        # запусить для игрока игру с начала
+        pass
+
+    def account_settings(self):
+        # смена аккаунта
+        self.account_settings_widget = SignInSignUp(self)
+
+    def exit_clicked(self):
+        # выход
+        self.close()
+
+    def update_current_player(self):
+        with open("data/qt/current_player.txt", "r", encoding="utf-8") as file:
+            data = file.readlines()
+            if data:
+                self.account_status_label.setText(f"Выполнен вход: {data[0]}")
+            else:
+                self.account_status_label.setText("Вход не выполнен")
+
+
+class SignInSignUp(QWidget):
+    def __init__(self, hello_screen):
+        self.hello_screen = hello_screen
+        super(SignInSignUp, self).__init__()
+        uic.loadUi("data/qt/sign_in_sign_up.ui", self)
+        self.init_ui()
+        self.show()
+
+    def init_ui(self):
+        self.setFixedSize(469, 112)
+        self.setWindowTitle("Смена аккаунта")
+
+        self.sign_in_widget = None
+        self.sign_up_widget = None
+
+        self.sign_in_button.clicked.connect(self.sign_in)
+        self.sign_up_button.clicked.connect(self.sign_up)
+
+    def sign_in(self):
+        self.close()
+        self.sign_in_widget = InputPlayerName("sign in", self.hello_screen)
+
+    def sign_up(self):
+        self.close()
+        self.sign_up_widget = InputPlayerName("sign up", self.hello_screen)
+
+
+class InputPlayerName(QWidget):
+    def __init__(self, type_of_call, hello_screen):
+        self.type_of_call = type_of_call
+        self.hello_screen = hello_screen
+        super(InputPlayerName, self).__init__()
+        uic.loadUi("data/qt/input_player_name.ui", self)
+        self.init_ui()
+        self.show()
+
+    def init_ui(self):
+        self.setWindowTitle("Смена аккаунта")
+
+        self.ok_button.clicked.connect(self.ok_pressed)
+
+    def ok_pressed(self):
+        player_name = self.player_name_lineedit.text()
+
+        # проверка что пользователь ввёл что-либо
+        if not player_name:
+            self.status_label.setText("Введите имя игрока")
+
+        # вход
+        elif self.type_of_call == "sign in":
+            if player_name_exists(player_name):
+                with open("data/qt/current_player.txt", "w", encoding="utf-8") as file:
+                    file.write(player_name)
+                self.hello_screen.update_current_player()
+                self.close()
+            else:
+                self.status_label.setText("Игрок не найден")
+
+        # регистрация
+        elif self.type_of_call == "sign up":
+            if not player_name_exists(player_name):
+                add_player_name(player_name)
+                with open("data/qt/current_player.txt", "w", encoding="utf-8") as file:
+                    file.write(player_name)
+                self.hello_screen.update_current_player()
+                self.close()
+            else:
+                self.status_label.setText("Данное имя уже используется")
+
+
+def init_hello_screen():
+    sys._excepthook = sys.excepthook
+
+    def exception_hook(exctype, value, traceback):
+        sys._excepthook(exctype, value, traceback)
+        sys.exit(1)
+
+    sys.excepthook = exception_hook
+
+    app = QApplication(sys.argv)
+    ex = HelloScreen()
+    sys.exit(app.exec())
+
+
+if __name__ == '__main__':
+    init_hello_screen()
