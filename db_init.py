569e0950968bc39cfc9a13d823eac0e8eef83185
Anna <alikinaan8@yandex.ru>
2022-01-01 20:00:02 +0500

    add player func player exists func create db

---
+++ db_init.py
@@ -0,0 +1,27 @@
+import sqlite3
+
+
+conn = sqlite3.connect('little_carrot.db')
+cur = conn.cursor()
+
+#create users table and fill data
+cur.execute("""CREATE TABLE IF NOT EXISTS users(
+   user_id INTEGER PRIMARY KEY AUTOINCREMENT,
+   player_name TEXT,
+   level INT,
+   map INT,
+   health INT);
+""")
+
+#create players
+#health?
+cur.execute("""INSERT INTO users(player_name, level, map, health)
+   VALUES('Anna', 1, 1, 0);""")
+
+cur.execute("""INSERT INTO users(player_name, level, map, health)
+   VALUES('Arthur', 1, 1, 0);""")
+
+cur.execute("""INSERT INTO users(player_name, level, map, health)
+   VALUES('Aleksey', 1, 1, 0);""")
+
+conn.commit()
--- main.py
+++ main.py
@@ -6,3 +6,4 @@ def kill_daddy(count_of_daddies):
     print("daddy is dead\n" * count_of_daddies, end='')
 
 print('Функция')
+
