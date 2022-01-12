f96bf0ba58c768a147584f83dc679d85cf56d0b5
Anna <alikinaan8@yandex.ru>
2022-01-12 23:04:36 +0500

    create func for add_hp, damage and get_current_hp

--- repository_db.py
+++ repository_db.py
@@ -31,3 +31,38 @@ def add_player(player_name):
         con.commit()
         cur.close()
         con.close()
+
+def get_current_hp(player_name):
+    con = sqlite3.connect('little_carrot.db')
+    cur = con.cursor()
+
+    try:
+        cur.execute(f"SELECT health FROM users WHERE player_name = '{player_name}'")
+        return cur.fetchone()[0]
+    finally:
+        cur.close()
+        con.close()
+
+def add_hp(player_name, count_of_add_hp):
+    con = sqlite3.connect('little_carrot.db')
+    cur = con.cursor()
+
+    try:
+        cur.execute( f"UPDATE users SET health = {count_of_add_hp + get_current_hp(player_name)} "
+                     f"WHERE player_name = '{player_name}'")
+    finally:
+        con.commit()
+        cur.close()
+        con.close()
+
+def damage_hp(player_name, count_of_damage_hp):
+    con = sqlite3.connect('little_carrot.db')
+    cur = con.cursor()
+
+    try:
+        cur.execute( f"UPDATE users SET health = {get_current_hp(player_name) - count_of_damage_hp} "
+                     f"WHERE player_name = '{player_name}'")
+    finally:
+        con.commit()
+        cur.close()
+        con.close()
